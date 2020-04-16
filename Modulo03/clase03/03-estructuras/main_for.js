3f6a336aa3af4354e0408e94cff1e3ef998dd39f
jtorriccelli <jtorriccelli1986@gmail.com>
2020-04-15 21:58:16 -0500

    update

---
+++ Modulo03/clase03/03-estructuras/main_for.js
@@ -0,0 +1,12 @@
+num=20;
+
+num++;
+
+num//21;
+
+for(var i=0;i<10;i++){
+	console.log(i);
+	// codigo que se repite tantas veces sea necesario.
+	console.log("fin operacion");
+
+}
---
+++ Modulo03/clase03/03-estructuras/main_if.js
@@ -0,0 +1,39 @@
+
+//var edad=prompt("ingresa tu edad");
+var edad=30;
+
+
+if(edad>=18){
+	console.log("La Persona es mayor de edad");
+  // se ejecuta si la condicion devuelve vedero;
+}
+else{
+	console.log("La persona es menor de edad");
+ // se ejecuta si la condicion devuelve falso;
+}
+
+var num1=10;
+//15 y 20
+if(num1>15){
+	console.log("El numero es mayor a 15");
+	if(num1<20){
+		// 
+		console.log("el numero se encuenttra en el rango de 15 a 20")
+	}
+	else{
+		console.log("Eres un numero mayor a 20");
+
+	}
+}
+else{
+	console.log("El numero es menor de 15");
+}
+
+
+
+if(num1>15 && num1<20){
+	console.log("el numero esta en el rango de 15 y 20")
+}
+else{
+	console.log("")
+}
