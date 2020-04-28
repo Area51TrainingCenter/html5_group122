99a0783bd80018775347629c8c1c2277a67f157b
jtorriccelli <jtorriccelli1986@gmail.com>
2020-04-27 22:06:04 -0500

    update

---
+++ Modulo03/clase06/json/js/main.js
@@ -0,0 +1,122 @@
+var objeto={
+	nombre:"Juan carlos",
+	correo:"correo@gmail.com",
+	codigo:1230001;
+	telefono:"999888777"
+}
+
+objeto.nombre;
+objeto.correo;
+
+
+
+
+
+"{nombre:'jc',apellido:'Ramos T. '}";
+
+{
+	colors:["white","Blue","Black","Green","Red"],
+	frutas:["Apple","Pineapple","Grapes","Melon"],
+}
+
+datos.colors // [5]
+datos.frutas // [4]
+
+
+
+`{
+	colors:["white","Blue","Black","Green","Red"],
+	frutas:["Apple","Pineapple","Grapes","Melon"],
+}`
+
+var pelicula={
+	nombre:"",
+	duracion:"",
+	actor:""
+}
+
+`{
+	pelicula1:{
+				nombre:"Pelicula1",
+				duracion:"1hora",
+				actor:"Actor1"
+			  },
+	pelicula2:{
+				nombre:"Pelicula2",
+				duracion:"2hora",
+				actor:"Actor2"
+			  },
+	pelicula3:{
+				nombre:"Pelicula3",
+				duracion:"3hora",
+				actor:"Actor3"
+			  }
+
+
+}`
+
+
+var resultado={
+	pelicula1:{
+				nombre:"Pelicula1",
+				duracion:"1hora",
+				actor:"Actor1"
+			  },
+	pelicula2:{
+				nombre:"Pelicula2",
+				duracion:"2hora",
+				actor:"Actor2"
+			  },
+	pelicula3:{
+				nombre:"Pelicula3",
+				duracion:"3hora",
+				actor:"Actor3"
+			  }
+
+
+}
+
+
+var peliculas={
+	titulo:"Listado de peliculas",
+	cantidad:10,
+	listado:[ 
+			{nombre:"Pelicula 1",duracion:"1 hora",actor:"Actor1"},
+			{nombre:"Pelicula 2",duracion:"2 horas",actor:"Actor2"},
+			{nombre:"Pelicula 3",duracion:"3 horas",actor:"Actor3"}
+    ]
+}
+
+
+`{
+	titulo:"Listado de peliculas",
+	cantidad:50,
+	listado:[ 
+			{nombre:"Pelicula 1",duracion:"1 hora",actor:"Actor1"},
+			{nombre:"Pelicula 2",duracion:"2 horas",actor:"Actor2"},
+			{nombre:"Pelicula 3",duracion:"3 horas",actor:"Actor3"}
+    ]
+}`
+
+respuesta.listado; // []
+
+for (var i=0;i<respuesta.cantidad;i++){
+	console.log(respuesta.listado[i])	
+}
+
+
+`[ 
+			{nombre:"Pelicula 1",duracion:"1 hora",actor:"Actor1"},
+			{nombre:"Pelicula 2",duracion:"2 horas",actor:"Actor2"},
+			{nombre:"Pelicula 3",duracion:"3 horas",actor:"Actor3"}
+    ]`
+
+
+  var respuesta[0]
+
+
+
+  4b8bc87b42a8a1eb7059930dcde9438b
+
+https://api.themoviedb.org/3/movie/popular?api_key=4b8bc87b42a8a1eb7059930dcde9438b&language=en-US&page=1
+
